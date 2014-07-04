648ffcdca085cb237f58703532b921f403641141
Andy Jiang <andyjiang@gmail.com>
2014-07-04 12:39:19 -0700

    changed the request

--- app/helpers/yo.js
+++ app/helpers/yo.js
@@ -6,11 +6,11 @@
   var path = 'http://api.justyo.co/yoall/';
   var YO_API_TOKEN = process.env.YO_API_TOKEN;
 
-  // content-type application/x-www-form-urlencoded
-
-  module.exports = {
-    yo: function yo () {
-      request.post(path).form({api_token: YO_API_TOKEN});
-    }
-  };
+  request.post({
+    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+    url:     path,
+    body:    YO_API_TOKEN
+  }, function(error, response, body){
+    console.log(body);
+  });
 }());
